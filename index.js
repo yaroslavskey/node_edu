7f194ea6f4ff921212351d8665d231c1d7df0725
yaroslavskey <yaroslavskey@ukr.net>
2022-10-28 19:27:12 +0300

    http_server_express

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+
+const port = 8000;
+const app = express();
+
+
+
+app.get('\hello', (req, res) => {
+    res.send('Привет');
+});
+
+app.post('\hello', (req, res) => {
+    res.send('Привет');
+});
+
+app.listen(port, () => {
+    console.log(`сервер запущен на 127.0.0.1 : ${port}`);
+});
+
